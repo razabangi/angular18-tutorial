2bd1652795b3ea345f6d56ceebacb573cd2837c9
razabangi <muhammadrazabangi9@gmail.com>
2024-09-18 00:38:09 +0500

    get all movies via http and display the table

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-movies',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.css'
 })
-export class MoviesComponent {
+export class MoviesComponent implements OnInit {
+  http = inject(HttpClient);
 
+  movieList: any = [];
+
+  ngOnInit(): void {
+    this.http.get('https://api.sampleapis.com/movies/comedy').subscribe((d: any) => {
+      this.movieList = d;
+    });
+  }
 }
